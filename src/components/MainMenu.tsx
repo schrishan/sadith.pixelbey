7abd813002192eb8fd45b65b864094f1cd267797
schrishan <48426302+schrishan@users.noreply.github.com>
2024-08-30 14:35:17 +0100

    update home page

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -6,19 +6,17 @@ export const MainMenu: React.FC = () => {
     return (
         <div className="sticky top-0 z-50 main-nav-bar">
             <div className="items-center backdrop-blur-2xl  h-[75px] p-4 w-full flex justify-center relative">
-                <div className="flex justify-center items-center">
+                <div className="logo mr-[5rem]">
+                    <span className="logo-txt text-2xl font-bold txt-gradient indent-0 p-4 textShine">Sadith Chrishantha</span>
+                </div>
+                <div className="flex justify-left items-center mr-[5rem]">
                     <div className="nav-bar flex gap-8 text-lg">
                         <a href="#" className="nav-itm px-2">About</a>
                         <a href="#" className="nav-itm px-2">Skills</a>
                         <a href="#" className="nav-itm px-2">Experience</a>
-                    </div>
-                    <div className="logo mx-10">
-                        <span className="logo-txt text-2xl font-bold txt-gradient indent-0 p-4 textShine">Sadith Chrishantha</span>
-                    </div>
-                    <div className="nav-bar flex gap-8 text-lg">
                         <a href="#" className="nav-itm px-2">Works</a>
                         <a href="#" className="nav-itm px-2">Let's connect</a>
-                        <a href="#" className="nav-itm px-2">Say Hi</a>
+                        <a href="#" className="nav-itm px-2">In their own words</a>
                     </div>
                 </div>
                 <div className="theme-swicher absolute right-4 top-[30%]"><ThemeSwitcher /></div>
